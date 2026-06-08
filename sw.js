e65aec9ecc16d6cc2f119ea7440e768ebdc30123
And-mar-94 <palagnukandrew94@gmail.com>
2026-06-08 22:26:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,27 @@
-const CACHE = 'gymbeast-v4';
+const CACHE = 'gymbeast-v5';
 const ASSETS = [
   '/gymbeast/',
   '/gymbeast/index.html',
   '/gymbeast/manifest.json',
   '/gymbeast/sw.js',
   '/gymbeast/icon-192.png',
-  '/gymbeast/icon-512.png'
+  '/gymbeast/icon-512.png',
+  '/gymbeast/e1_0.jpg','/gymbeast/e1_1.jpg',
+  '/gymbeast/e2_0.jpg','/gymbeast/e2_1.jpg',
+  '/gymbeast/e3_0.jpg','/gymbeast/e3_1.jpg',
+  '/gymbeast/e4_0.jpg','/gymbeast/e4_1.jpg',
+  '/gymbeast/e5_0.jpg','/gymbeast/e5_1.jpg',
+  '/gymbeast/e6_0.jpg','/gymbeast/e6_1.jpg',
+  '/gymbeast/e7_0.jpg','/gymbeast/e7_1.jpg',
+  '/gymbeast/e8_0.jpg','/gymbeast/e8_1.jpg',
+  '/gymbeast/e9_0.jpg','/gymbeast/e9_1.jpg',
+  '/gymbeast/e10_0.jpg','/gymbeast/e10_1.jpg',
+  '/gymbeast/e11_0.jpg','/gymbeast/e11_1.jpg',
+  '/gymbeast/e12_0.jpg','/gymbeast/e12_1.jpg',
+  '/gymbeast/e13_0.jpg','/gymbeast/e13_1.jpg',
+  '/gymbeast/e14_0.jpg','/gymbeast/e14_1.jpg',
+  '/gymbeast/e15_0.jpg','/gymbeast/e15_1.jpg',
+  '/gymbeast/e16_0.jpg','/gymbeast/e16_1.jpg',
 ];
 
 self.addEventListener('install', e => {
